ad8a8f0f5aefa4533482f805b4632d1cf98ebb94
abraham-villalba <abraham.villalba@encora.com>
2025-04-01 15:26:11 -0700

    Added request validation and basic error handling

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,37 @@
 from flask import Flask
 from app.routes import api_bp
+from app.utils.response_model import ResponseModel
+
+def register_error_handlers(app):
+    """ Registers error handlers for the Flask app. """
+    @app.errorhandler(404)
+    def not_found(_):
+        return ResponseModel(status='error', error={'code': 404, 'message': 'Not Found'}).to_json(), 404
+    
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return ResponseModel(status='error', error={'code': 500, 'message': 'Internal Server Error'}).to_json(), 500
+    
+    @app.errorhandler(400)
+    def bad_request(_):
+        return ResponseModel(status='error', error={'code': 400, 'message': 'Bad Request'}).to_json(), 400
+    
+    @app.errorhandler(405)
+    def method_not_allowed(_):
+        return ResponseModel(status='error', error={'code': 405, 'message': 'Method Not Allowed'}).to_json(), 405
+    
+    @app.errorhandler(415)
+    def unsupported_media_type(_):
+        return ResponseModel(status='error', error={'code': 415, 'message': 'Unsupported Media Type'}).to_json(), 415
+    
+    # In case of any other error, return a generic error message
+    @app.errorhandler(Exception)
+    def generic_error(error):
+        return ResponseModel(status='error', error={'code': 500, 'message': 'Internal Server Error'}).to_json(), 500
+
 
 def create_app():
+    """ Creates a Flask app instance. """
     app = Flask(__name__)
 
     # Load configurations
@@ -9,5 +39,7 @@ def create_app():
 
     # Register the Blueprints
     app.register_blueprint(api_bp, url_prefix='/api')
+
+    register_error_handlers(app)
 
     return app
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -2,6 +2,8 @@ from flask import Blueprint, request, jsonify
 from langchain.prompts import ChatPromptTemplate
 from langchain_ollama.llms import OllamaLLM
 from langchain_openai import ChatOpenAI
+from app.utils.schemas import ChatRequestSchema, validate_chat_request
+from app.utils.response_model import ResponseModel
 
 template = """
 You are a support agent. You have been assigned to help a customer with a query.
@@ -15,10 +17,17 @@ api_bp = Blueprint('api', __name__)
 
 @api_bp.route('/chat', methods=['POST'])
 def chat():
-    model = OllamaLLM(model="llama3.2")
+    """ Handles chatbot queries. """
+    data, errors = validate_chat_request(request.get_json(), ChatRequestSchema())
+
+    if errors:
+        return ResponseModel(status="error", error=errors).to_json(), 400 # Bad Request
+
+    model = OllamaLLM(model='llama3.2')
     chain = prompt | model
-    data = request.get_json()
+    query = data['query'] #"Hello, my name is Abraham"
     print(data)
-    result = chain.invoke({"question": "How do I reset my password?"})
+    print(query)
+    result = chain.invoke({'question': query})
     print(result)
-    return jsonify({'response': result})
+    return ResponseModel(status='success', data={'response': result}).to_json(), 200
